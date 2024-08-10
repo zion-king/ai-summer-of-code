54a2741789f82fc4170b52038e4efd5e95b88130
pibzion <zion.pibowei@periculum.io>
2024-08-10 18:10:42 +0100

    AISOC tutorials: Week 1 day 1

---
+++ week 1/tutorials/day_1_first_llm/simple.py
@@ -0,0 +1,49 @@
+"""Build a simple LLM application"""
+
+import os
+import groq
+from dotenv import load_dotenv
+load_dotenv()
+
+# Set GRO_API_KEY = "your api key" in the .env file, then load it below
+GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
+groq_client = groq.Groq(api_key = GROQ_API_KEY)
+
+sys_prompt = """You are a helpful virtual assistant. \
+    Your goal is to provide useful and relevant \
+        responses to my request"""
+
+models = [
+    "llama-3.1-405b-reasoning",
+    "llama-3.1-70b-versatile",
+    "llama-3.1-8b-instant",
+    "mixtral-8x7b-32768"
+]
+
+
+def generate(model, query, temperature=0):
+
+    response = groq_client.chat.completions.create(
+        model = model,
+        messages = [
+            {"role": "system", "content": sys_prompt},
+            {"role": "user", "content": query},
+        ],
+        response_format = {"type": "text"},
+        temperature = temperature
+    )
+
+    answer = response.choices[0].message.content
+
+    return answer
+
+
+if __name__ == "__main__":
+    model = models[2]
+    query = "which is bigger? 9.11 or 9.9?"
+    response = generate(model, query, temperature=1)
+    print(response)
+
+
+
+
